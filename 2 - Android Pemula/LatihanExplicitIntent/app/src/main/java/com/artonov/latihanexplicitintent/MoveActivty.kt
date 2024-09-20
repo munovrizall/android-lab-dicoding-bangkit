faf70f3cbfc5699d54e9dcafca3f93557215406f
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-09-20 22:55:59 +0700

    Latihan Explicit Intent completed

---
+++ 2 - Android Pemula/LatihanExplicitIntent/app/src/main/java/com/artonov/latihanexplicitintent/MoveActivty.kt
@@ -0,0 +1,15 @@
+package com.artonov.latihanexplicitintent
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MoveActivty : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_activty)
+
+    }
+}
