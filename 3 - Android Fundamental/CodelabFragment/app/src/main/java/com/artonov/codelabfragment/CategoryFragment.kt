5b01fa7a2f14acf9ffa07f86a31651245da48723
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-09-25 19:07:06 +0700

    Codelab fragment completed

---
+++ 3 - Android Fundamental/CodelabFragment/app/src/main/java/com/artonov/codelabfragment/CategoryFragment.kt
@@ -0,0 +1,45 @@
+package com.artonov.codelabfragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+class CategoryFragment : Fragment(), View.OnClickListener {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnDetailCategory: Button = view.findViewById(R.id.btn_detail_category)
+        btnDetailCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_detail_category) {
+            val detailCategoryFragment = DetailCategoryFragment()
+
+            val bundle = Bundle()
+            bundle.putString(DetailCategoryFragment.EXTRA_NAME, "Lifestyle")
+            detailCategoryFragment.arguments = bundle
+
+            val description = "Kategori ini akan berisi produk-produk lifestyle"
+            detailCategoryFragment.description = description
+
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, detailCategoryFragment, DetailCategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
+    }
+}
---
+++ 3 - Android Fundamental/CodelabFragment/app/src/main/java/com/artonov/codelabfragment/ProfileActivity.kt
@@ -0,0 +1,16 @@
+package com.artonov.codelabfragment
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+
+    }
+}
