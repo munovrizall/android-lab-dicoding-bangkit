c5d84923904db17910c8d8100c355d9c5d6672cb
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-09-26 13:00:03 +0700

    Codelab navigation completed

---
+++ 3 - Android Fundamental/CodelabNavigation/app/src/main/java/com/artonov/codelabnavigation/DetailCategoryFragment.kt
@@ -0,0 +1,42 @@
+package com.artonov.codelabnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.artonov.codelabnavigation.databinding.FragmentDetailCategoryBinding
+
+class DetailCategoryFragment : Fragment() {
+
+    private var _binding:FragmentDetailCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentDetailCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
+        val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
+
+        binding.tvCategoryName.text = dataName
+        binding.tvCategoryDescription.text = "Stock : $dataDescription"
+
+        binding.btnHome.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
+        )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
