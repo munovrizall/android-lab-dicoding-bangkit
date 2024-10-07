e78d9e75833453dfcd2c875b9fb4cc658e5f5ffa
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-10-07 20:08:17 +0700

    codelab viewmodel completed

---
+++ 3 - Android Fundamental/CodelabViewmodel/app/src/main/java/com/artonov/codelabviewmodel/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.artonov.codelabviewmodel
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import com.artonov.codelabviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener() {
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+        }
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
+    }
+}
