88b8fcb6602c8fa4da6f2d2b237ae7b6b85c152e
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-10-09 14:47:13 +0700

    Codelab livedata

---
+++ 3 - Android Fundamental/CodelabLivedata/app/src/main/java/com/artonov/codelablivedata/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.artonov.codelablivedata
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.artonov.codelablivedata.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val liveDataTimerViewModel: MainViewModel by viewModels()
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        subscribe()
+    }
+
+    private fun subscribe() {
+        val elapsedTimeObserver = Observer<Long?> { aLong ->
+            val newText = this@MainActivity.resources.getString(R.string.seconds, aLong)
+            binding.timerTextview.text = newText
+        }
+
+        liveDataTimerViewModel.getElapsedTime().observe(this, elapsedTimeObserver)
+    }
+}
