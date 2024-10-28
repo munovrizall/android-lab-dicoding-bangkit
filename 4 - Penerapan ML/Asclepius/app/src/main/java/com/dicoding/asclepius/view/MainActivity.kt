f6932e220c3a0fbf7f7e5773f03e2403ef01ead3
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-10-28 21:48:17 +0700

    Initial setup completed

---
+++ 4 - Penerapan ML/Asclepius/app/src/main/java/com/dicoding/asclepius/view/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.dicoding.asclepius.view
+
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.dicoding.asclepius.R
+import com.dicoding.asclepius.databinding.ActivityMainBinding
+import com.yalantis.ucrop.UCrop
+import java.io.File
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var currentImageUri: Uri? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.galleryButton.setOnClickListener{
+            startGallery()
+        }
+    }
+
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK && result.data != null) {
+            val selectedImageUri = result.data?.data
+            selectedImageUri?.let { startCrop(it) }
+        }
+    }
+
+    private val cropLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK && result.data != null) {
+            val resultUri = UCrop.getOutput(result.data!!)
+            resultUri?.let { uri ->
+                binding.previewImageView.setImageURI(uri)
+            }
+        } else if (result.resultCode == UCrop.RESULT_ERROR) {
+            val cropError = UCrop.getError(result.data!!)
+            cropError?.let {
+                showToast("Crop Error: ${it.message}")
+            }
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        galleryLauncher.launch(intent)
+    }
+
+    private fun startCrop(uri: Uri) {
+        val destinationUri = Uri.fromFile(File(cacheDir, "cropped_image.jpg"))
+        val uCrop = UCrop.of(uri, destinationUri)
+            .withAspectRatio(1f, 1f)
+            .withMaxResultSize(500, 500)
+
+        cropLauncher.launch(uCrop.getIntent(this))
+    }
+
+    private fun showImage() {
+        // TODO: Menampilkan gambar sesuai Gallery yang dipilih.
+    }
+
+    private fun analyzeImage() {
+        // TODO: Menganalisa gambar yang berhasil ditampilkan.
+    }
+
+    private fun moveToResult() {
+        val intent = Intent(this, ResultActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ 4 - Penerapan ML/Asclepius/settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://www.jitpack.io" ) }// Tambahkan baris ini untuk JitPack
+    }
+}
+
+
+rootProject.name = "Asclepius"
+include(":app")
+ 
