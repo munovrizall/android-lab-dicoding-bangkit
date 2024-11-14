4c17940dfa52af6d98ffffe8646baa8832e7c872
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-11-15 00:04:44 +0700

    Codelab background service completed

---
+++ 5 - Android Intermediate/CodelabService/app/src/main/java/com/artonov/codelabservice/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.artonov.codelabservice
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.artonov.codelabservice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val serviceIntent = Intent(this, MyBackgroundService::class.java)
+
+        binding.btnStartBackgroundService.setOnClickListener {
+            startService(serviceIntent)
+        }
+
+        binding.btnStopBackgroundService.setOnClickListener() {
+            stopService(serviceIntent)
+        }
+    }
+}
