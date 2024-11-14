afa4f756d164b97289491b83305a7478ec3a8171
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-11-15 00:43:03 +0700

    Codelab foreground service completed

--- 5 - Android Intermediate/CodelabService/app/src/main/java/com/artonov/codelabservice/MainActivity.kt
+++ 5 - Android Intermediate/CodelabService/app/src/main/java/com/artonov/codelabservice/MainActivity.kt
@@ -1,15 +1,29 @@
 package com.artonov.codelabservice
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.artonov.codelabservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean? ->
+        if (!isGranted!!)
+            Toast.makeText(this,
+                "Unable to display Foreground service notification due to permission decline",
+                Toast.LENGTH_LONG)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +38,25 @@ class MainActivity : AppCompatActivity() {
 
         binding.btnStopBackgroundService.setOnClickListener() {
             stopService(serviceIntent)
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED)
+                requestPermissionLauncher.launch(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        val foregroundServiceIntent = Intent(this, MyForegroundService::class.java)
+        binding.btnStartForegroundService.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(foregroundServiceIntent)
+            } else {
+                startService(foregroundServiceIntent)
+            }
+        }
+
+        binding.btnStopForegroundService.setOnClickListener {
+            stopService(foregroundServiceIntent)
         }
     }
 }
