f62967ea882a921389de126223dca7be2d71aad9
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-11-18 23:38:42 +0700

    feat(auth): register ui completed

---
+++ 5 - Android Intermediate/TaleNet/app/src/main/java/com/artonov/talenet/ui/register/RegisterActivity.kt
@@ -0,0 +1,26 @@
+package com.artonov.talenet.ui.register
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.artonov.talenet.R
+import com.artonov.talenet.databinding.ActivityRegisterBinding
+import com.artonov.talenet.ui.login.LoginActivity
+
+class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
