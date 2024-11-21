20f62999a236fa4aee8e4588082e7a7fb0d6e069
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2024-11-22 00:02:14 +0700

    feat(detail): added sharedelement animation

--- 5 - Android Intermediate/TaleNet/app/src/main/java/com/artonov/talenet/ui/story/StoryAdapter.kt
+++ 5 - Android Intermediate/TaleNet/app/src/main/java/com/artonov/talenet/ui/story/StoryAdapter.kt
@@ -1,17 +1,21 @@
 package com.artonov.talenet.ui.story
 
+import android.app.Activity
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.app.ActivityOptionsCompat
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.artonov.talenet.R
 import com.artonov.talenet.data.response.ListStoryItem
 import com.artonov.talenet.databinding.ItemStoryCardBinding
 import com.artonov.talenet.ui.story_detail.StoryDetailActivity
 import com.bumptech.glide.Glide
+import androidx.core.util.Pair
 
 class StoryAdapter : ListAdapter<ListStoryItem, StoryAdapter.ViewHolder>(DIFF_CALLBACK) {
     class ViewHolder(private val binding: ItemStoryCardBinding) :
@@ -39,11 +43,18 @@ class StoryAdapter : ListAdapter<ListStoryItem, StoryAdapter.ViewHolder>(DIFF_CA
         holder.bind(story)
 
         holder.itemView.setOnClickListener() {
+            val optionsCompat: ActivityOptionsCompat =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    holder.itemView.context as Activity,
+                    Pair(holder.itemView.findViewById(R.id.iv_item_photo), "profile"),
+                    Pair(holder.itemView.findViewById(R.id.tv_item_name), "name"),
+                )
+
             val context = holder.itemView.context
 
             val intent = Intent(context, StoryDetailActivity::class.java)
             intent.putExtra(StoryDetailActivity.EXTRA_STORY_ID, story.id)
-            startActivity(context, intent, null)
+            startActivity(context, intent, optionsCompat.toBundle())
         }
     }
 
