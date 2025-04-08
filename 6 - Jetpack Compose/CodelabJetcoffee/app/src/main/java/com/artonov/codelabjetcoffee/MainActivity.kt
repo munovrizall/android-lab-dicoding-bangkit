a75045977723ef77011124cca4d28712025ff386
Mohammad Novrizal <muhammadnovrizal87@gmail.com>
2025-04-08 22:11:21 +0700

    Latihan bottom nav completed

--- 6 - Jetpack Compose/CodelabJetcoffee/app/src/main/java/com/artonov/codelabjetcoffee/MainActivity.kt
+++ 6 - Jetpack Compose/CodelabJetcoffee/app/src/main/java/com/artonov/codelabjetcoffee/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.artonov.codelabjetcoffee
 
-import android.R.attr.category
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,13 +9,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,6 +32,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.artonov.codelabjetcoffee.model.BottomBarItem
 import com.artonov.codelabjetcoffee.model.Menu
 import com.artonov.codelabjetcoffee.model.dummyBestSellerMenu
 import com.artonov.codelabjetcoffee.model.dummyCategory
@@ -35,7 +41,6 @@ import com.artonov.codelabjetcoffee.ui.components.CategoryItem
 import com.artonov.codelabjetcoffee.ui.components.HomeSection
 import com.artonov.codelabjetcoffee.ui.components.MenuItem
 import com.artonov.codelabjetcoffee.ui.components.Search
-import com.artonov.codelabjetcoffee.ui.components.SectionText
 import com.artonov.codelabjetcoffee.ui.theme.CodelabJetcoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,20 +57,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp(modifier: Modifier = Modifier) {
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Banner()
-        HomeSection(
-            title = stringResource(R.string.section_category),
-            content = { CategoryRow() }
-        )
-        HomeSection(
-            title = stringResource(R.string.menu_favorite),
-            content = { MenuRow(dummyMenu) }
-        )
-        HomeSection(
-            title = stringResource(R.string.section_best_seller_menu),
-            content = { MenuRow(dummyBestSellerMenu) }
-        )
+    Scaffold(
+        bottomBar = { BottomBar() }
+    ) { contentPadding ->
+        Column(
+            modifier = Modifier
+                .padding(contentPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            Banner()
+            HomeSection(
+                title = stringResource(R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(
+                title = stringResource(R.string.menu_favorite),
+                content = { MenuRow(dummyMenu) }
+            )
+            HomeSection(
+                title = stringResource(R.string.section_best_seller_menu),
+                content = { MenuRow(dummyBestSellerMenu) }
+            )
+        }
     }
 }
 
@@ -112,6 +125,46 @@ fun MenuRow(listMenu: List<Menu>, modifier: Modifier = Modifier) {
     }
 }
 
+@Composable
+fun BottomBar(modifier: Modifier = Modifier) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            ),
+        )
+        navigationItems.map {
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+
+                },
+                label = {
+                    Text(it.title)
+                },
+                selected =
+                    it.title == navigationItems[0].title,
+                onClick = {}
+            )
+        }
+    }
+}
+
 @Preview(showBackground = true, device = Devices.PIXEL_4)
 @Composable
 fun JetCoffeeAppPreview() {
@@ -128,7 +181,6 @@ fun CategoryRowPreview() {
     }
 }
 
-
 @Composable
 @Preview(showBackground = true)
 fun MenuRowPreview() {
@@ -136,5 +188,13 @@ fun MenuRowPreview() {
         MenuRow(
             listMenu = dummyMenu,
         )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun BottomBarPreview() {
+    CodelabJetcoffeeTheme {
+        BottomBar()
     }
 }
